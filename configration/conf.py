59fc19bcb7b503f84a66bbf2e7e1a0fe6c6b4dba
Aboobacker Bilal P <aboobackerbilalp@gmail.com>
2024-10-23 22:24:11 +0530

    [FIX]: modified config file

--- configration/conf.py
+++ configration/conf.py
@@ -1,11 +1,18 @@
+from pydantic.v1 import BaseSettings
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from const import BASE_URLS
 
-uri = BASE_URLS
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+class Settings(BaseSettings):
+    MONGO_URL: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
+
+client = MongoClient(settings.MONGO_URL, server_api=ServerApi('1'))
 
 db = client.todo_db
 collection = db["todo_data"]
